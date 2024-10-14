a48c67d16a00a89c9a63e730ac643d076ee24791
Ade Rahmat Maulana <adermaulana15@gmail.com>
2024-10-14 21:15:44 +0800

    update

--- index.php
+++ index.php
@@ -1,163 +1,222 @@
-<?php
-
-    include 'koneksi.php';
-
-    session_start();
-
-    if(isset($_SESSION['status']) == 'login'){
-
-        header("location:admin");
-    }
-
-    if(isset($_POST['login'])){
-
-        $username = $_POST['username'];
-        $password = md5($_POST['password']);
-
-        $login = mysqli_query($koneksi, "SELECT * FROM admin WHERE username='$username' and password='$password'");
-        $cek = mysqli_num_rows($login);
-
-        if($cek > 0) {
-            $admin_data = mysqli_fetch_assoc($login);
-            $_SESSION['id_admin'] = $admin_data['id'];
-            $_SESSION['nama_admin'] = $admin_data['nama'];
-            $_SESSION['username_admin'] = $username;
-            $_SESSION['status'] = "login";
-            header('location:admin');
-
-        }  else {
-            echo "<script>
-            alert('Login Gagal, Periksa Username dan Password Anda!');
-            header('location:index.php');
-                 </script>";
-        }
-    }
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-  <title>Pages / Login - NiceAdmin Bootstrap Template</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+  <title>Antrian BRI</title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="https://upload.wikimedia.org/wikipedia/commons/2/2e/BRI_2020.svg" rel="icon">
+  <link href="https://upload.wikimedia.org/wikipedia/commons/2/2e/BRI_2020.svg" rel="apple-touch-icon">
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+  <link href="assets/home/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/home/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/home/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/home/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/home/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <!-- Main CSS File -->
+  <link href="assets/home/css/main.css" rel="stylesheet">
 
   <!-- =======================================================
-  * Template Name: NiceAdmin - v2.5.0
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Template Name: Vesperr
+  * Template URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+  * Updated: Aug 07 2024 with Bootstrap v5.3.3
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
 </head>
 
-<body>
+<body class="index-page">
 
-  <main>
-    <div class="container">
+  <header id="header" class="header d-flex align-items-center sticky-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center">
 
-      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+      <a href="index.php" class="logo d-flex align-items-center me-auto">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <img src="https://upload.wikimedia.org/wikipedia/commons/2/2e/BRI_2020.svg">
+        
+      </a>
 
-              <div class="d-flex justify-content-center py-4">
-                <a href="index.html" class="logo d-flex align-items-center w-auto">
-                  <img src="assets/img/logo.png" alt="">
-                  <span class="d-none d-lg-block">NiceAdmin</span>
-                </a>
-              </div><!-- End Logo -->
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="index.php" class="active">Home<br></a></li>
+          <li><a href="#services">Layanan</a></li>
+          <li><a href="#faq">Faq</a></li>
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
 
-              <div class="card mb-3">
-
-                <div class="card-body">
-
-                  <div class="pt-4 pb-2">
-                    <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                    <p class="text-center small">Enter your username & password to login</p>
-                  </div>
-
-                  <form class="row g-3 needs-validation" novalidate method="POST">
-
-                    <div class="col-12">
-                      <label for="yourUsername" class="form-label">Username</label>
-                      <div class="input-group has-validation">
-                        <span class="input-group-text" id="inputGroupPrepend">@</span>
-                        <input type="text" name="username" class="form-control" id="yourUsername" required>
-                        <div class="invalid-feedback">Please enter your username.</div>
-                      </div>
-                    </div>
-
-                    <div class="col-12">
-                      <label for="yourPassword" class="form-label">Password</label>
-                      <input type="password" name="password" class="form-control" id="yourPassword" required>
-                      <div class="invalid-feedback">Please enter your password!</div>
-                    </div>
-
-
-                    <div class="col-12">
-                      <button class="btn btn-primary w-100" type="submit" name="login">Login</button>
-                    </div>
-
-                  </form>
-
-                </div>
-              </div>
-
-              <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-              </div>
-
-            </div>
-          </div>
-        </div>
-
-      </section>
+      <a class="btn-getstarted" href="login.php">Login</a>
 
     </div>
-  </main><!-- End #main -->
+  </header>
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <main class="main">
+    <!-- Services Section -->
+    <section id="services" class="services section">
+
+      <!-- Section Title -->
+      <div class="container section-title" data-aos="fade-up">
+        <h2>Pilih Layanan</h2>
+      </div><!-- End Section Title -->
+    </section><!-- /Services Section -->
+
+    <!-- Alt Services Section -->
+    <section id="alt-services" style="margin-top:-150px;" class="alt-services section">
+
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="row gy-4">
+
+          <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="200">
+            <div class="service-item position-relative">
+              <div class="img">
+                <img src="assets/home/img/services-1.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="details">
+                <a href="antrian.php" class="stretched-link">
+                  <h3>Ambil Antrian</h3>
+                </a>
+              </div>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="300">
+            <div class="service-item position-relative">
+              <div class="img">
+                <img src="assets/home/img/services-2.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="details">
+                <a href="service-details.html" class="stretched-link">
+                  <h3>Lihat Antrian</h3>
+                </a>
+              </div>
+            </div>
+          </div><!-- End Service Item -->
+
+        </div>
+
+      </div>
+
+    </section><!-- /Alt Services Section -->
+
+    <!-- Faq Section -->
+    <section id="faq" class="faq section light-background">
+
+      <!-- Section Title -->
+      <div class="container section-title" data-aos="fade-up">
+        <h2>Frequently Asked Questions</h2>
+        <p>Sistem Antrian Bank</p>
+      </div><!-- End Section Title -->
+
+      <div class="container">
+
+        <div class="row faq-item" data-aos="fade-up" data-aos-delay="100">
+          <div class="col-lg-5 d-flex">
+            <i class="bi bi-question-circle"></i>
+            <h4>Apa itu sistem antrian bank?</h4>
+          </div>
+          <div class="col-lg-7">
+            <p>
+                Sistem antrian bank adalah aplikasi yang membantu mengelola dan memantau giliran pelanggan saat menunggu layanan di bank, memastikan pelayanan lebih teratur dan efisien.
+            </p>
+          </div>
+        </div><!-- End F.A.Q Item-->
+
+        <div class="row faq-item" data-aos="fade-up" data-aos-delay="200">
+          <div class="col-lg-5 d-flex">
+            <i class="bi bi-question-circle"></i>
+            <h4>Bagaimana cara mengambil nomor antrian?</h4>
+          </div>
+          <div class="col-lg-7">
+            <p>
+            Anda dapat mengambil nomor antrian melalui mesin antrian di bank, aplikasi mobile bank (jika tersedia), atau melalui website resmi bank untuk beberapa cabang yang mendukung layanan online.
+            </p>
+          </div>
+        </div><!-- End F.A.Q Item-->
+
+        <div class="row faq-item" data-aos="fade-up" data-aos-delay="300">
+          <div class="col-lg-5 d-flex">
+            <i class="bi bi-question-circle"></i>
+            <h4>Bisakah saya mengambil nomor antrian sebelum datang ke bank?</h4>
+          </div>
+          <div class="col-lg-7">
+            <p>
+            Ya, beberapa bank menyediakan fitur untuk mengambil nomor antrian secara online melalui aplikasi mobile atau website, sehingga Anda bisa mengambil nomor dari rumah sebelum datang ke cabang
+            </p>
+          </div>
+        </div><!-- End F.A.Q Item-->
+
+        <div class="row faq-item" data-aos="fade-up" data-aos-delay="400">
+          <div class="col-lg-5 d-flex">
+            <i class="bi bi-question-circle"></i>
+            <h4>Apa yang terjadi jika saya terlambat saat nomor antrian saya dipanggil?</h4>
+          </div>
+          <div class="col-lg-7">
+            <p>
+            Jika Anda terlambat dan nomor Anda sudah dipanggil, Anda mungkin diminta untuk mengambil nomor antrian baru atau menunggu sampai ada waktu yang memungkinkan untuk dipanggil kembali.
+            </p>
+          </div>
+        </div><!-- End F.A.Q Item-->
+
+      </div>
+
+    </section><!-- /Faq Section -->
+
+
+  </main>
+
+  <footer id="footer" class="footer">
+
+    <div class="container">
+      <div class="copyright text-center ">
+        <p>© <span>Copyright</span> <strong class="px-1 sitename">Vesperr</strong> <span>All Rights Reserved</span></p>
+      </div>
+      <div class="social-links d-flex justify-content-center">
+        <a href=""><i class="bi bi-twitter-x"></i></a>
+        <a href=""><i class="bi bi-facebook"></i></a>
+        <a href=""><i class="bi bi-instagram"></i></a>
+        <a href=""><i class="bi bi-linkedin"></i></a>
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you've purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+
+  </footer>
+
+  <!-- Scroll Top -->
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Preloader -->
+  <div id="preloader"></div>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/chart.js/chart.umd.js"></script>
-  <script src="assets/vendor/echarts/echarts.min.js"></script>
-  <script src="assets/vendor/quill/quill.min.js"></script>
-  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
-  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/home/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/home/vendor/php-email-form/validate.js"></script>
+  <script src="assets/home/vendor/aos/aos.js"></script>
+  <script src="assets/home/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/home/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/home/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/home/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="assets/home/vendor/isotope-layout/isotope.pkgd.min.js"></script>
 
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <!-- Main JS File -->
+  <script src="assets/home/js/main.js"></script>
 
 </body>
 
